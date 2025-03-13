e6272a76297eb6da606b872ad68a296513646d3e
yuki <38411009+yukijina@users.noreply.github.com>
2025-03-13 07:21:20 -0700

    created function to move with a dashed line

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-print(timmy_the_turtle)
+speedy = Turtle()
+print(speedy)
 
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
+speedy.shape("turtle")
+speedy.color("red")
+
+def make_dashline(line_length, dash_length, num):
+  for _ in range(0,num):
+    speedy.pendown()
+    speedy.forward(line_length)
+    speedy.penup()
+    speedy.forward(dash_length)
 
 
-def make_square():
-  for _ in range(0,4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+make_dashline(10, 5, 10)
+
+# def make_square():
+#   for _ in range(0,4):
+#     speedy.forward(100)
+    #speedy.right(90)
 
 
-make_square()
+#make_square()
 
 screen = Screen()
 screen.exitonclick()
